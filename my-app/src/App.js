d5b4c15cb592bf700abd323b1208186690f850ce
MartinaP87 <bleach1987@hotmail.it>
2023-01-02 12:58:20 +0000

    Class components

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+function App() {
+  return (
+    <div className="App">
+      <StatefulGreetingWithPrevState/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = (props) =>{
+    console.log(props);
+    return <h1>Hello, {props.greeting} {props.name}! You are {props.age} years old</h1>;
+}
+
+
+export default FunctionalGreetingWithProps;
